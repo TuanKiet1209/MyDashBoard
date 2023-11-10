dc04fa00be071b94311eab1c4e1d35498e9884ed
TuanKiet1209 <tuankietlth@gmail.com>
2023-11-10 20:16:47 +0700

    reponsive dashboard

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Kim-yoo-Jung',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Lee Jong Suk',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Yoona',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
